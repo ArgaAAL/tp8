f22df0196e4fd83d002cf300cbe06c58d330744d
ArgaAAL <argalumunon9@gmail.com>
2024-04-14 15:27:59 +0700

    menyelesaikan program dengan runtime configuration

--- tpmodul8_1302223038/Program.cs
+++ tpmodul8_1302223038/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        CovidConfig config = new CovidConfig();
+
+        Console.WriteLine($"Berapa suhu badan anda saat ini? Dalam nilai {config.SatuanSuhu}");
+        double suhu = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam?");
+        int hariDeman = int.Parse(Console.ReadLine());
+
+        if ((config.SatuanSuhu == "celcius" && suhu >= 36.5 && suhu <= 37.5) ||
+            (config.SatuanSuhu == "fahrenheit" && suhu >= 97.7 && suhu <= 99.5) &&
+            hariDeman < config.BatasHariDemam)
+        {
+            Console.WriteLine(config.PesanDiterima);
+        }
+        else
+        {
+            Console.WriteLine(config.PesanDitolak);
+        }
+
+        config.UbahSatuan();
+        Console.WriteLine($"Dalam penggunaan program selanjutnya, akan digunakan satuan {config.SatuanSuhu}");
+    }
+}
--- tpmodul8_1302223038/obj/Debug/net8.0/tpmodul8_1302223038.AssemblyInfo.cs
+++ tpmodul8_1302223038/obj/Debug/net8.0/tpmodul8_1302223038.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul8_1302223038")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cf734383457b0d17c9c3410fa6a1860ce595565")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul8_1302223038")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul8_1302223038")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
